c80f03f04bd4e0b7510a4bba8cef85f5c71ca87f
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2020-07-11 09:08:30 +0300

    Add repository package with ProductRepository.java and ProductInMemoryRepository.java

---
+++ src/lv/sbogdano/javaguru/shoppinglist/repository/ProductInMemoryRepository.java
@@ -0,0 +1,25 @@
+package lv.sbogdano.javaguru.shoppinglist.repository;
+
+import lv.sbogdano.javaguru.shoppinglist.domain.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ProductInMemoryRepository implements ProductRepository {
+
+    private final Map<Long, Product> repository = new HashMap<>();
+    private long productId = 0L;
+
+    @Override
+    public Product save(Product product) {
+        product.setId(productId++);
+        repository.put(product.getId(), product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return Optional.ofNullable(repository.get(id));
+    }
+}
---
+++ src/lv/sbogdano/javaguru/shoppinglist/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package lv.sbogdano.javaguru.shoppinglist.repository;
+
+import lv.sbogdano.javaguru.shoppinglist.domain.Product;
+
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    Product save(Product product);
+
+    Optional<Product> getProductById(Long id);
+}
